a9d040430573aec25ade127e5aa5f4300a5a7cfe
champix56 <desorbaix@free.fr>
2022-11-07 15:28:31 +0100

    creation d'un cmp

---
+++ App/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+function App(props) {
+  return (
+    <View>
+      <Text>DEMAT BREIZH</Text>
+    </View>
+  );
+}
+export default App;
---
+++ App/App.styles.js
@@ -0,0 +1,11 @@
+import {StyleSheet} from 'react-native';
+
+const AppStylesheet = StyleSheet.create({
+  textBlue: {
+    color: 'skyblue',
+  },
+  underline: {
+    textDecoration: 'underline',
+  },
+});
+export default AppStylesheet;
