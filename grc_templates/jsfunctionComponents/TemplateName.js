4b1f2e834a8305a72ff8d2f120e9e227df137b58
champix56 <desorbaix@free.fr>
2022-11-07 16:33:55 +0100

    passage en arrow du template

--- grc_templates/jsfunctionComponents/TemplateName.js
+++ grc_templates/jsfunctionComponents/TemplateName.js
@@ -2,11 +2,11 @@ import React from 'react';
 import {Text, View} from 'react-native';
 import TemplateNameStylesheet from './TemplateName.styles';
 
-function TemplateName(props) {
+const TemplateName = props => {
   return (
     <View style={TemplateNameStylesheet.TemplateName} testID="TemplateName">
       <Text>templateName</Text>
     </View>
   );
-}
+};
 export default TemplateName;
