fdab6516aa7326610b213def7938b4e74745ab6f
champix56 <desorbaix@free.fr>
2022-11-07 15:37:00 +0100

    usage de classe(eq.) simple

--- App/App.js
+++ App/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import {Text, View} from 'react-native';
+import AppStylesheet from './App.styles';
 
 function App(props) {
   return (
     <View>
-      <Text style={{color: 'blue', textDecoration: 'underline'}}>
+      <Text style={AppStylesheet.textBlue}>
         DEMAT BREIZH
       </Text>
     </View>
