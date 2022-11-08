0f351b7bdaea167390f07bff35dde3609bdb3047
champix56 <desorbaix@free.fr>
2022-11-08 17:07:40 +0100

    gestion appel rest sans store avec diffusion  aux enfants

--- App/App.js
+++ App/App.js
@@ -5,18 +5,29 @@ import Button from './components/Button/Button';
 import ButtonClass from './components/ButtonClass/ButtonClass';
 import Produits from './components/Produits/Produits';
 import Splash from './components/Splash/Splash';
-import {produits} from '../db.json'
+
 function App(props) {
   const [screen, setscreen] = useState(null);
-  
+  const [produits, setproduits] = useState([]);
   useEffect(() => {
-    if (screen === null) {
-      setscreen(<Splash />);
-      setTimeout(() => {
-        setscreen(<Produits produits={produits} />);
-      }, 5000);
-    }
+    //const pr1 =
+    fetch(
+      'https://my-json-server.typicode.com/champix56/frncv1-2022-11-07/produits',
+    )
+      .then(f => f.json())
+      .then(a => {
+        setproduits(a);
+        console.log(a);
+      });
+    //const pr2 = new Promise(() => setTimeout(() => {}), 2000);
+    //Promise.race([pr1,pr2]).then()
+
+    setscreen(<Splash />);
   }, []);
+  useEffect(() => {
+    if (produits.length > 0) setscreen(<Produits produits={produits} />);
+  }, [produits]);
+
   return screen;
 }
 export default App;
