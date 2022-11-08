66c2e0c8608a2d8ff9f6042a12b5fbaabd613b4d
champix56 <desorbaix@free.fr>
2022-11-08 09:30:14 +0100

    conservation des highlight/opactity
    Signed-off-by: champix56 <desorbaix@free.fr>

--- App/components/Button/Button.js
+++ App/components/Button/Button.js
@@ -1,12 +1,14 @@
 import React from 'react';
-import {Text, TouchableHighlight} from 'react-native';
+import {Text, TouchableHighlight, View} from 'react-native';
 import ButtonStylesheet from './Button.styles';
 
 const Button = props => {
   console.log(props);
   return (
-    <TouchableHighlight style={ButtonStylesheet.Button} testID="Button">
-      <Text style={ButtonStylesheet.contentText}>{props.textDuButton}</Text>
+    <TouchableHighlight  testID="Button">
+      <View style={ButtonStylesheet.Button}>
+        <Text style={ButtonStylesheet.contentText}>{props.textDuButton}</Text>
+      </View>
     </TouchableHighlight>
   );
 };
