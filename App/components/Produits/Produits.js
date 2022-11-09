5395099c60bda7a1ea5d67bfb260f343bcdad7e2
champix56 <desorbaix@free.fr>
2022-11-09 12:57:43 +0100

    ajout du search produit dans fenetre produits

--- App/components/Produits/Produits.js
+++ App/components/Produits/Produits.js
@@ -3,10 +3,12 @@ import {View} from 'react-native';
 import ProduitsScrollContainer from '../ProduitsScrollContainer/ProduitsScrollContainer';
 import ProduitsStylesheet from './Produits.styles';
 import PropTypes from 'prop-types';
+import SearchProduit from '../SearchProduit/SearchProduit';
 
 const Produits = props => {
   return (
     <View style={ProduitsStylesheet.Produits} testID="Produits">
+      <SearchProduit />
       <ProduitsScrollContainer />
     </View>
   );
