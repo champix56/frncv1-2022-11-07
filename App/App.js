939d238afe7a81403f730399264961071fd38ff4
champix56 <desorbaix@free.fr>
2022-11-09 10:28:43 +0100

    ajout du provider de context de store

--- App/App.js
+++ App/App.js
@@ -6,28 +6,34 @@ import ButtonClass from './components/ButtonClass/ButtonClass';
 import Produits from './components/Produits/Produits';
 import Splash from './components/Splash/Splash';
 import {store} from './store/produit.reducer';
+import {Provider} from 'react-redux';
 function App(props) {
   console.log(store);
   const [screen, setscreen] = useState(null);
-  const [produits, setproduits] = useState([]);
+  // const [produits, setproduits] = useState([]);
   useEffect(() => {
     //const pr1 =
-    fetch(
-      'https://my-json-server.typicode.com/champix56/frncv1-2022-11-07/produits',
-    )
-      .then(f => f.json())
-      .then(a => {
-        setproduits(a);
-        console.log(a);
-      });
+    // fetch(
+    //   'https://my-json-server.typicode.com/champix56/frncv1-2022-11-07/produits',
+    // )
+    //   .then(f => f.json())
+    //   .then(a => {
+    //     setproduits(a);
+    //     console.log(a);
+    //   });
     //const pr2 = new Promise(() => setTimeout(() => {}), 2000);
     //Promise.race([pr1,pr2]).then()
 
-    setscreen(<Splash />);
+    //setscreen(<Splash />);
+    setscreen(
+      <Provider store={store}>
+        <Produits />
+      </Provider>,
+    );
   }, []);
-  useEffect(() => {
-    if (produits.length > 0) setscreen(<Produits produits={produits} />);
-  }, [produits]);
+  // useEffect(() => {
+  //   if (produits.length > 0) setscreen(<Produits produits={produits} />);
+  // }, [produits]);
 
   return screen;
 }
