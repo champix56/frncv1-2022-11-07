72318c119ba971fa6d3290a90642a4002aee9ef8
champix56 <desorbaix@free.fr>
2022-11-09 16:54:16 +0100

    force devtool redux

--- App/store/store.js
+++ App/store/store.js
@@ -2,6 +2,7 @@ import {configureStore, combineReducers} from '@reduxjs/toolkit';
 import produitReducer from './produits.reducer';
 export const store = configureStore({
   reducer: combineReducers({produits: produitReducer}),
+  devTools: true,
 });
 /*  produits:
         produits,currentProduit
