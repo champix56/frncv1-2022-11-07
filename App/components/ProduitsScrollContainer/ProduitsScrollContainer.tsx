0b1923288e9b4b4962481d09b684ef21d638dc12
champix56 <desorbaix@free.fr>
2022-11-09 10:07:19 +0100

    mapsState+mapDispatch connect au store avec preservation de cmp deconnecté

--- App/components/ProduitsScrollContainer/ProduitsScrollContainer.tsx
+++ App/components/ProduitsScrollContainer/ProduitsScrollContainer.tsx
@@ -1,34 +1,44 @@
-import React, {useEffect, useState} from 'react';
+import React from 'react';
 import {ScrollView, View} from 'react-native';
 import I_Produit from '../../interfaces/produit';
-import {store} from '../../store/store';
+import {connect} from 'react-redux';
 import ProduitThumbnail from '../ProduitThumbnail/ProduitThumbnail';
 import ProduitsScrollContainerStylesheet from './ProduitsScrollContainer.styles';
+import {selectCurrent} from '../../store/produits.reducer';
 
 interface I_ProduitsScrollContainerProps {
   produits: Array<I_Produit>;
 }
-const initialProduits: Array<I_Produit> = [];
 const ProduitsScrollContainer: React.FunctionComponent<
   I_ProduitsScrollContainerProps
 > = props => {
-  //connexion "old school d'un cmp"
-  const [produits, setproduits] = useState(initialProduits);
-  useEffect(() => {
-    store.subscribe(() => {
-      setproduits(store.getState().produits.produits);
-    });
-  }, []);
-
   return (
     <ScrollView
       style={ProduitsScrollContainerStylesheet.ProduitsScrollContainer}>
       <View style={ProduitsScrollContainerStylesheet.mainView}>
-        {produits.map((e, i) => (
+        {props.produits.map((e, i) => (
           <ProduitThumbnail key={`produit${i}`} {...e} />
         ))}
       </View>
     </ScrollView>
   );
 };
-export default ProduitsScrollContainer;
+//I_ProduitsScrollContainerOwnProps
+// {produits:{produits:[],currentProduits:{}}}
+function mapStateToProps(state: any, ownProps: any) {
+  return {
+    ...ownProps,
+    produits: state.produits.produits,
+  };
+}
+function mapDispatchToProps(dispatch: Function) {
+  return {
+    onPressProduit: (id: number) => dispatch(selectCurrent(id)),
+  };
+}
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps,
+)(ProduitsScrollContainer);
+
+export const unconnectedProduitScrollContainer = ProduitsScrollContainer;
