d074124190b6c41b59d3aaadd77e499a93eeba12
champix56 <desorbaix@free.fr>
2022-11-09 12:46:28 +0100

    action de filter sur la liste de produits

--- App/store/produits.reducer.js
+++ App/store/produits.reducer.js
@@ -1,8 +1,16 @@
-import {createSlice} from '@reduxjs/toolkit';
+import {createSlice, createAsyncThunk} from '@reduxjs/toolkit';
 import {produits} from '../../db.json';
+/*export interface I_ProduitsState{
+    fullListProduits:Array<I_Produit>;
+    produits:Array<I_Produit>;
+    currentProduit:I_Produit;
+    loading: "idle" | "pending" | "succeeded" | "failed";
+}*/
 export const initialState = {
-  produits: produits,
+  fullListProduits: [],
+  produits: [],
   currentProduit: null,
+  loading: 'idle',
 };
 
 const produitSlice = createSlice({
@@ -18,9 +26,38 @@ const produitSlice = createSlice({
     selectCurrent: (state, id) => {
       state.currentProduit = state.produits.find(e => e.id === id);
     },
+    filterProduits: (s, str) => {
+      s.produits = s.fullListProduits.filterProduits(e => e.nom.includes(str));
+    },
+  },
+  extraReducers: builder => {
+    builder.addCase(fetchProduits.fulfilled, (state, payload) => {
+      state.fullListProduits = payload.payload;
+      state.produits = state.fullListProduits;
+      state.loading = 'succeeded';
+    });
+    builder.addMatcher(
+      action =>
+        action.type.includes('produits/fetch/') &&
+        !action.type.includes('fulfilled'),
+      (state, payload) => {
+        console.log(payload);
+        state.produits = [];
+        state.fullListProduits = [];
+      },
+    );
   },
 });
 
+export const fetchProduits = createAsyncThunk('produits/fetch', async () => {
+  console.log('%c%s', 'color:red', 'fetch start');
+  const result = await fetch(
+    'https://my-json-server.typicode.com/champix56/frncv1-2022-11-07/produits',
+  );
+  const response = await result.json();
+  console.log(response);
+  return response;
+});
 export const {addProduit, addProduits, selectCurrent} = produitSlice.actions;
 
 export default produitSlice.reducer;
