51a211b29fa640b3cf73b52d262f3319b7358689
champix56 <desorbaix@free.fr>
2022-11-09 14:47:21 +0100

    connected search

--- App/components/SearchProduit/SearchProduit.js
+++ App/components/SearchProduit/SearchProduit.js
@@ -1,19 +1,23 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import {Text, TextInput, View} from 'react-native';
+import {filterProduits} from '../../store/produits.reducer';
 import SearchProduitStylesheet from './SearchProduit.styles';
-
+import {useDispatch} from 'react-redux';
 const SearchProduit = props => {
-  const [searchStr, setsearchStr] = useState('');
   return (
     <View style={SearchProduitStylesheet.SearchProduit} testID="SearchProduit">
       <TextInput
         placeholder="recherche"
-        value={searchStr}
         onChangeText={value => {
-          setsearchStr(value);
+          props.onSearch(value);
         }}
       />
     </View>
   );
 };
-export default SearchProduit;
+
+export const UnconncetedSearchProduit = SearchProduit;
+export default props => {
+  const d = useDispatch();
+  return <SearchProduit onSearch={str => d(filterProduits(str))} />;
+};
