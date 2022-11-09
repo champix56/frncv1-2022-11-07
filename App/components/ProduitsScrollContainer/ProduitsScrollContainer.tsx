e402155e93193ca119bf67a9767f13612f75c4f7
champix56 <desorbaix@free.fr>
2022-11-09 09:55:29 +0100

    connexion par subscribe

--- App/components/ProduitsScrollContainer/ProduitsScrollContainer.tsx
+++ App/components/ProduitsScrollContainer/ProduitsScrollContainer.tsx
@@ -1,20 +1,30 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {ScrollView, View} from 'react-native';
 import I_Produit from '../../interfaces/produit';
+import {store} from '../../store/store';
 import ProduitThumbnail from '../ProduitThumbnail/ProduitThumbnail';
 import ProduitsScrollContainerStylesheet from './ProduitsScrollContainer.styles';
 
 interface I_ProduitsScrollContainerProps {
   produits: Array<I_Produit>;
 }
+const initialProduits: Array<I_Produit> = [];
 const ProduitsScrollContainer: React.FunctionComponent<
   I_ProduitsScrollContainerProps
 > = props => {
+  //connexion "old school d'un cmp"
+  const [produits, setproduits] = useState(initialProduits);
+  useEffect(() => {
+    store.subscribe(() => {
+      setproduits(store.getState().produits.produits);
+    });
+  }, []);
+
   return (
     <ScrollView
       style={ProduitsScrollContainerStylesheet.ProduitsScrollContainer}>
       <View style={ProduitsScrollContainerStylesheet.mainView}>
-        {props.produits.map((e, i) => (
+        {produits.map((e, i) => (
           <ProduitThumbnail key={`produit${i}`} {...e} />
         ))}
       </View>
